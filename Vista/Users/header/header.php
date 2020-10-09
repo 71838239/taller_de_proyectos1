ad1a809ea42e26272c1b073f2c85bb262f875fd0
71838239 <56099716+71838239@users.noreply.github.com>
2020-10-09 13:15:59 -0500

    Create header.php

---
+++ Vista/Users/header/header.php
@@ -0,0 +1,13 @@
+                <div class="header__contenedor container-fluid">
+                    
+
+                    <!-- <a href="javascript:void(0);" class="sidebarCollapse" data-placement="bottom"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-menu"><line x1="3" y1="12" x2="21" y2="12"></line><line x1="3" y1="6" x2="21" y2="6"></line><line x1="3" y1="18" x2="21" y2="18"></line></svg></a>-->
+                    <img class="header__logo rounded-circle" alt="logo" src="./img/syslogo.png" href="<?php echo URL; ?>" id="" role="button">
+                            
+                    <div class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100">
+                        <a class="header__titulo" href="<?php echo URL; ?>">
+                            <span class="navbar-brand-name h3 mb-0 " href="<?php echo URL; ?>" role="button">Mesa de Partes Virtual</span>
+                        </a>
+                    </div>
+                        
+                </div>
